b4e1b96976f96b59ca1c9a7160e32982b12cdbec
Carey99 <edwinakidah1@gmail.com>
2025-01-28 17:06:47 +0300

    Rune literals in Go

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"Golang/greeting"
+	"fmt"
 )
 
 func main() {
---
+++ rune_literals.go
@@ -0,0 +1,13 @@
+//strings are enclosed using double quotes in go
+//single quotes are reserved for rune literals
+//a rune in Go represents a single unicode point
+
+package main
+
+import "fmt"
+
+func main() {
+	r := 'A'
+
+	fmt.Println(r)
+}
